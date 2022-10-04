3b8b77b182e9f1cd75bdfeac932a0ed14d99cd48
Yandex Master <you@yandex.com>
2022-10-04 15:25:43 +0100

    recoded

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const device = require('express-device');
 const puppeteer = require('puppeteer')
 
 const PORT = process.env.PORT || 5000;
@@ -13,9 +12,8 @@ app.disable('x-powered-by');
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
-app.use(device.capture());
 
-app.use("/api/webshot", async (req, res) => {
+app.use("/api/", async (req, res) => {
   const generateID = () => {
     const ID = new Date().getTime().toString(36);
     return ID;
@@ -27,8 +25,6 @@ app.use("/api/webshot", async (req, res) => {
         error: "URL is required",
       });
     }
-
-    console.log(req.device.type)
 
     // get token from parameter
     const token = req.query.token;
@@ -43,73 +39,6 @@ app.use("/api/webshot", async (req, res) => {
       __dirname,
       "./public/" + ID + ".png"
     );
-
-    // capture webpage according to device
-
-    if (req.device.type === "phone") {
-      // launch headless brpwser
-      const browser = await puppeteer.launch(
-        {
-          args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-          ],
-        }
-      );
-
-      // Create a new page
-      const page = await browser.newPage();
-
-      // Navigate to some website
-      await page.goto(`https://${url}`, {
-        waitUntil: "load",
-        // Remove the timeout
-        timeout: 0,
-      });
-
-      await page.setViewport({
-        width: 400,
-        height: 731,
-      });
-
-      await page.screenshot({ path: imageStorage });
-
-      await browser.close();
-
-      return res.sendFile(imageStorage);
-    }
-
-    // tablet view screenshot
-    if (req.device.type === "tablet") {
-      // launch headless brpwser
-      const browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ],
-      });
-
-      // Create a new page
-      const page = await browser.newPage();
-
-      // Navigate to some website
-      await page.goto(`https://${url}`, {
-        waitUntil: "load",
-        // Remove the timeout
-        timeout: 0,
-      });
-
-      await page.setViewport({
-        width: 768,
-        height: 1024,
-      });
-
-      await page.screenshot({ path: imageStorage });
-
-      await browser.close();
-
-      return res.sendFile(imageStorage);
-    }
 
     // launch headless brpwser
     const browser = await puppeteer.launch({
@@ -128,6 +57,11 @@ app.use("/api/webshot", async (req, res) => {
       timeout: 0,
     });
 
+    await page.setViewport({
+      width: 768,
+      height: 1024,
+    });
+
     await page.screenshot({ path: imageStorage });
 
     await browser.close();
@@ -136,15 +70,8 @@ app.use("/api/webshot", async (req, res) => {
   } catch (error) {
     console.log(error.message);
 
-    res.status(500).json({erroe: error.message});
+    res.status(500).json({ erroe: error.message });
   }
 });
-
-app.use("/api", (req, res) => {
-  res.status(200).json({
-    success: true,
-    db: true,
-  });
-})
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
